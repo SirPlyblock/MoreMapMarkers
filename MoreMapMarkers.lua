739be471c057852ea851d2f1e9d72efc5c33ffad
SirPlyblock <jonas@levinsson.se>
2025-06-24 19:53:07 +0200

    UIFrame complete

--- MoreMapMarkers.lua
+++ MoreMapMarkers.lua
@@ -1,5 +1,6 @@
 -- Marker data: { continent, zoneID, x, y, name, type, info, Atlas ID }
-local points = {
+local playerfaction = ""
+local puntas = {
     -- Kalimdor Mailboxes
     {1, 26, 0.622, 0.392, "Mailbox", "mail", "", nil}, -- Ratchet
     {1, 12, 0.749, 0.440, "Mailbox", "mail", "", nil }, --Camp Mojache
@@ -33,6 +34,8 @@ local points = {
     { 2, 24, 0.434, 0.415, "Mailbox", "mail", "", nil },--Silverpine Forest
     { 2, 27, 0.454, 0.551, "Mailbox", "mail", "", nil },--Swamp of Sorrows
     { 2, 30, 0.789, 0.805, "Mailbox", "mail", "", nil },--The Hinterlands
+    { 2, 12, 0.429, 0.655, "Mailbox", "mail", "", nil },--Elwynn Forest
+    { 2, 35, 0.531, 0.533, "Mailbox", "mail", "", nil },--Westfall
     -- Reagent vendors in Eastern Kingfom
     {2, 33, 0.828, 0.158, "Reagent Vendor", "reag", "Hannah Akeley", "Horde"}, --Undercity
     { 1, 20, 0.454, 0.565, "Reagent Vendor", "reag", "Horthus", "Horde" },--Orgrimmar
@@ -63,118 +66,13 @@ local points = {
     { 2, 1, 0.434, 0.604, "Flight Master", "flight", "Voryn Skystrider", "Alliance" },
     { 2, 25, 0.709, 0.725, "Flight Master", "flight", "Dungar Longdrink", "Alliance" }, --Stormwind City
     { 2, 27, 0.461, 0.548, "Flight Master", "flight", "Breyk", "Horde" }, --Swamp of Sorrows
+    { 2, 20, 0.306, 0.594, "Flight Master", "flight", "Ariena Stormfeather", "Alliance" },--Redridge Mountains
     { 2, 30, 0.817, 0.817, "Flight Master", "flight", "Gorkas", "Horde" } --The Hinterlands
 }
 
--- local function CreateMapMarkerUI()
---     locationProperties = CreateFrame("Frame", "MoarMarkerInfo", UIParent)
---     locationProperties:SetWidth(320)
---     locationProperties:SetHeight(220)
---     locationProperties:SetPoint("CENTER", UIParent, "CENTER")
---     locationProperties:SetBackdrop({
---         bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
---         edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
---         tile = true,
---         tileSize = 32,
---         edgeSize = 32,
---         insets = {
---             left = 11,
---             right = 11,
---             top = 11,
---             bottom = 11
---         }
---     })
---     locationProperties:SetMovable(true)
---     locationProperties:EnableMouse(true)
---     locationProperties:RegisterForDrag("LeftButton")
---     locationProperties:SetScript("OnDragStart", function()
---         this:StartMoving()
---     end)
---     config:SetScript("OnDragStop", function()
---         this:StopMovingOrSizing()
---     end)
-    
---     local title = config:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
---     title:SetPoint("TOP", 0, -15)
---     title:SetText("Add Map Marker Location")
-
---     -- Flight point
---     local flightPointLabel = config:CreateFontString(nil, "OVERLAY", "GameFontNormal")
---     flightPointLabel:SetPoint("TOPLEFT", 20, -45)
---     flightPointLabel:SetText("Flight point")
---     local flightPointCheckBox = CreateFrame("CheckButton", nil, config, "UICheckButtonTemplate")
---     flightPointCheckBox:SetPoint("LEFT", flightPointLabel, "RIGHT", 5, 0)
---     flightPointCheckBox:SetWidth(24)
---     flightPointCheckBox:SetHeight(24)
---     flightPointCheckBox:SetScript("OnClick", function()
---         local checked = flightPointCheckBox:GetChecked()
---         if checked then
---             mailboxCheckBox:SetChecked(false)
---             reagentCheckBox:SetChecked(false)
---         end
---     end)
-
---     -- Mailbox
---     local mailboxLabel = config:CreateFontString(nil, "OVERLAY", "GameFontNormal")
---     mailboxLabel:SetPoint("TOPLEFT", 20, -25)
---     mailboxLabel:SetText("Mailbox")
---     local mailboxCheckBox = CreateFrame("CheckButton", nil, config, "UICheckButtonTemplate")
---     mailboxCheckBox:SetPoint("LEFT", mailboxLabel, "RIGHT", 5, 0)
---     mailboxCheckBox:SetWidth(24)
---     mailboxCheckBox:SetHeight(24)
---     mailboxCheckBox:SetScript("OnClick", function()
---         local checked = mailboxCheckBox:GetChecked()
---         if checked then
---             flightPointCheckBox:SetChecked(false)
---             reagentCheckBox:SetChecked(false)
---         end
---     end)
-
---     -- Reagents
---     local reagentLabel = config:CreateFontString(nil, "OVERLAY", "GameFontNormal")
---     reagentLabel:SetPoint("TOPLEFT", 20, -5)
---     reagentLabel:SetText("Reagents")
---     local reagentCheckBox = CreateFrame("CheckButton", nil, config, "UICheckButtonTemplate")
---     reagentCheckBox:SetPoint("LEFT", reagentLabel, "RIGHT", 5, 0)
---     reagentCheckBox:SetWidth(24)
---     reagentCheckBox:SetHeight(24)
---     reagentCheckBox:SetScript("OnClick", function()
---         local checked = reagentCheckBox:GetChecked()
---         if checked then
---             flightPointCheckBox:SetChecked(false)
---             reagentCheckBox:SetChecked(false)
---         end
---     end)
-
-
-
---     -- Add category toggles
---     -- dungeonRaidsToggle = CreateToggleCheckbox(config, 20, -75, "Show Dungeons & Raids", "showDungeonRaids")
---     -- transportToggle = CreateToggleCheckbox(config, 20, -100, "Show Transport (Boats, Zeppelins, Trams)", "showTransport")
---     -- worldBossToggle = CreateToggleCheckbox(config, 20, -125, "Show World Bosses", "showWorldBosses")
-
---     local closeButton = CreateFrame("Button", nil, config, "UIPanelButtonTemplate")
---     closeButton:SetWidth(80)
---     closeButton:SetHeight(25)
---     closeButton:SetPoint("BOTTOM", 0, 15)
---     closeButton:SetText("Ok")
---     closeButton:SetScript("OnClick", function()
---         locationProperties:Hide()
---     end)
---     local cancelButton = CreateFrame("Button", nil, config, "UIPanelButtonTemplate")
---     closeButton:SetWidth(80)
---     closeButton:SetHeight(25)
---     closeButton:SetPoint("BOTTOM", 0, 15)
---     closeButton:SetText("Cancel")
---     closeButton:SetScript("OnClick", function()
---         locationProperties:Hide()
---     end)
---     -- Hide the config window by default
---     config:Hide()
--- end
-
 -- keeping zoneIDs for reference and debugging only
 kZoneNames = {GetMapZones(1)}
+
 ekZoneNames = {GetMapZones(2)}
 local firstLoad = true
 
@@ -182,9 +80,9 @@ local markers = {}
 local debug = false
 
 local function CreateMapPin(parent, x, y, size, texture, tooltipText, tooltipInfo)
-    if debug then
-        print("Attempting to create map pin. X: " .. x .. " Y: " .. y .. " Size: ".. size .. " Texture: " .. texture .. " Tip Text: " .. tooltipText)
-    end
+    -- if debug then
+    --     print("Attempting to create map pin. X: " .. x .. " Y: " .. y .. " Size: ".. size .. " Texture: " .. texture .. " Tip Text: " .. tooltipText)
+    -- end
     local pin = CreateFrame("Button", nil, parent)
     pin.texture = pin:CreateTexture(nil, "OVERLAY")
     pin:SetWidth(size)
@@ -225,14 +123,14 @@ local function UpdateMarkers()
 
     if currentZone == 0 and currentContinent == 0 then
         -- When Continent and Zone ID is 0 We are looking at the zoomed out world map
-        if debug then
-            print("Zone and Continent index is 0 - Looking at World Map")
-        end
+        -- if debug then
+        --     print("Zone and Continent index is 0 - Looking at World Map")
+        -- end
     elseif currentZone == 0 then
         -- When Zone ID is 0 We are looking at the zoomed out map of a continent
-        if debug then
-            print("Looking at Continent Map")
-        end
+        -- if debug then
+        --     print("Looking at Continent Map")
+        -- end
     end    
     
     -- Because these maps also have co-ordinates, we need to remove any drawn pins otherwise they will overlay these maps
@@ -245,24 +143,24 @@ local function UpdateMarkers()
 
     local worldMap = WorldMapDetailFrame
     local mapWidth, mapHeight = worldMap:GetWidth(), worldMap:GetHeight()
-    local playerfaction = UnitFactionGroup("player")
-    for i, data in pairs(points) do
+    
+    for i, data in pairs(MoreMapMarkersDB.points) do
         local isMatching = false
         local cont, zoneID, x, y, label, kind, info, faction = unpack(data)   
 
-        if debug then
-            print("Cont: " .. cont .. " Zone ID: " .. zoneID .. " X: " .. x .. " Y: " .. y .. " Label: " .. label .. " Kind: " .. kind)
-            -- We are looking at Kalimdor or Eastern Kingdoms (not the world map)
-            print("Current Zone ID is: " .. currentZone)
-        end
+        -- if debug then
+        --     print("Cont: " .. cont .. " Zone ID: " .. zoneID .. " X: " .. x .. " Y: " .. y .. " Label: " .. label .. " Kind: " .. kind)
+        --     -- We are looking at Kalimdor or Eastern Kingdoms (not the world map)
+        --     print("Current Zone ID is: " .. currentZone)
+        -- end
         
         if currentZone == zoneID and currentContinent == cont and (playerfaction == faction or faction == nil) then
             isMatching = true
-            if debug then
-                print("Matched current continent " .. currentContinent .. " to pin data for zone: " .. label)
-                print("We are looking at a map for zone: " .. currentZone .. " which matches defined zone pin data:" ..
-                        zoneID .. " for zone: " .. label)
-            end
+            -- if debug then
+            --     print("Matched current continent " .. currentContinent .. " to pin data for zone: " .. label)
+            --     print("We are looking at a map for zone: " .. currentZone .. " which matches defined zone pin data:" ..
+            --             zoneID .. " for zone: " .. label)
+            -- end
         end
         
         if isMatching then
@@ -288,37 +186,154 @@ local function UpdateMarkers()
     end
 end
 
+local function InsertInMarkerTable(c, z, x, y, description, ttype, targetname, faction)
+    assert(type(c) == "number", "Continent must be a number (1 or 2)")
+    assert(x >= 0 and x <= 1 and y >= 0 and y <= 1, "Coordinates must be 0-1")
+    table.insert(MoreMapMarkersDB.points, 
+    {
+        c,  -- Eastern Kingdoms (1 = Kalimdor)
+        z, -- Zone ID (Undercity = 33)
+        x, y,  -- X, Y coordinates
+        description,  -- Display name
+        ttype,  -- Type ("mail", "reag", "flight")
+        targetname and tostring(targetname) or "",  -- Additional info (e.g., NPC name)
+        faction  -- Faction restriction (nil for both)
+    })
+end
+
+local function GetPlayerWorldLocation()
+    local c,z=GetCurrentMapContinent(),GetCurrentMapZone() 
+    local x,y=GetPlayerMapPosition("player") 
+    return c,z,x,y  
+end
+
+local function flightButtonClicked()
+    local c,z,x,y = GetPlayerWorldLocation()
+    local t = UnitName("target") or ""
+    InsertInMarkerTable(c, z, x, y, "Flight Master" ,"flight", t, playerfaction)
+    UpdateMarkers()
+    DEFAULT_CHAT_FRAME:AddMessage(string.format([[{ %d, %d, %.3f, %.3f, "Flight Master", "flight", "%s", "%s" },--%s]],c,z,x,y,t,playerfaction,GetZoneText()))
+    DEFAULT_CHAT_FRAME:AddMessage("Flight point added")
+end
+
+local function mailButtonClicked()
+    local c,z,x,y = GetPlayerWorldLocation()
+    InsertInMarkerTable(c, z, x, y, "Mailbox", "mail", "", nil)
+    UpdateMarkers()
+    DEFAULT_CHAT_FRAME:AddMessage(string.format([[{ %d, %d, %.3f, %.3f, "Mailbox", "mail", "", nil },--%s]],c,z,x,y,GetZoneText()))
+    DEFAULT_CHAT_FRAME:AddMessage("Mailbox Added")
+end
+
+local function reagentButtonClicked()
+    local c,z,x,y = GetPlayerWorldLocation()
+    local t = UnitName("target") or ""
+    InsertInMarkerTable(c, z, x, y, "Reagent Vendor", "reag", t, playerfaction)
+    UpdateMarkers()
+    DEFAULT_CHAT_FRAME:AddMessage(string.format([[{ %d, %d, %.3f, %.3f, "Reagent Vendor", "reag", "%s", "%s" },--%s]],c,z,x,y,t,playerfaction,GetZoneText()))
+    DEFAULT_CHAT_FRAME:AddMessage("Reagent vendor added")
+end
+
+local function CreateButtonFrame(parentframe, anchor, xpos, ypos, siz, texture, delegate)
+    local butt = CreateFrame("Button", nil, parentframe)
+    butt:SetWidth(siz)
+    butt:SetHeight(siz)
+    butt:SetPoint(anchor, parentframe, anchor, xpos, ypos)
+
+    -- Set the button's texture
+    local buttonTexture = butt:CreateTexture(nil, "BACKGROUND")
+    buttonTexture:SetTexture(texture)
+    buttonTexture:SetAllPoints()
+    if delegate then
+        butt:SetScript("OnClick", delegate)
+    end
+
+    return button
+end
+
+local function CreateMapMarkerUI()
+    MoreMapMarkerFrame = CreateFrame("Frame", "MoreMapMarkerUI", UIParent)
+    MoreMapMarkerFrame:SetWidth(98)
+    MoreMapMarkerFrame:SetHeight(48)
+    local pos = MoreMapMarkersDB.framePosition or 
+    {
+        x = 0, y = 0
+    }
+    MoreMapMarkerFrame:SetPoint("CENTER", UIParent, "CENTER", pos.x, pos.y)
+    MoreMapMarkerFrame:SetBackdrop({
+        bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+        --edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+        tile = true,
+        tileSize = 32,
+        edgeSize = 0,
+        insets = {
+            left = 0,
+            right = 0,
+            top = 0,
+            bottom = 0
+        }
+    })
+    MoreMapMarkerFrame:SetMovable(true)
+    MoreMapMarkerFrame:EnableMouse(true)
+    MoreMapMarkerFrame:RegisterForDrag("LeftButton")
+    MoreMapMarkerFrame:SetScript("OnDragStart", function()
+        this:StartMoving()
+    end)
+    MoreMapMarkerFrame:SetScript("OnDragStop", function()
+        this:StopMovingOrSizing()
+        local point, _, relativePoint, x, y = this:GetPoint()
+        MoreMapMarkersDB.framePosition = 
+        {
+            x = x,
+            y = y,
+            point = point,
+            relativePoint = relativePoint
+        }
+    end)
+
+    -- Add create POI-buttons
+    local flightButton = CreateButtonFrame(MoreMapMarkerFrame, "BOTTOMLEFT", 1, 1, 32, "Interface\\Addons\\MoreMapMarkers\\Images\\taxi.tga", flightButtonClicked)
+    local mailButton = CreateButtonFrame(MoreMapMarkerFrame, "BOTTOMLEFT", 34, 1, 32, "Interface\\Addons\\MoreMapMarkers\\Images\\mail.tga", mailButtonClicked)
+    local vendorButton = CreateButtonFrame(MoreMapMarkerFrame, "BOTTOMLEFT", 67, 1, 32, "Interface\\Addons\\MoreMapMarkers\\Images\\icon_vendor.tga", reagentButtonClicked)
+    --local closeButton = CreateButtonFrame(MoreMapMarkerFrame,"TOPRIGHT", 1, 1, 10, "Interface\\Addons\\MoreMapMarkers\\Images\\close.tga", function() MoreMapMarkerFrame:Hide() end)
+    MoreMapMarkerFrame:Hide()
+end
+
 -- Add a flag to track if we've already initialized
 local initialized = false
 
 -- Event(s) handling frame
 local frame = CreateFrame("Frame")
+MoreMapMarkersDB = MoreMapMarkersDB or
+{ 
+    points = puntas,
+    FrameVisible = true
+}
 
 frame:RegisterEvent("WORLD_MAP_UPDATE")
---frame:RegisterEvent("VARIABLES_LOADED")
+frame:RegisterEvent("VARIABLES_LOADED")
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 frame:SetScript("OnEvent", function()
     if event == "ADDON_LOADED" and arg1 == "MoreMapMarkers" then
-        -- Addon is loaded, create UI but don't initialize saved vars yet
-        --eateMapMarkerUI()
---    elseif event == "VARIABLES_LOADED" then
+        
+        DEFAULT_CHAT_FRAME:AddMessage("More Map Markers loaded")
+    elseif event == "VARIABLES_LOADED" then
         -- This is when saved variables are actually available
---        if not initialized then
---            UpdateCheckboxStates()
---            initialized = true
---            
---            if debug then
---                print("Modern Map Markers: Variables Loaded and Initialized")
---            end
---        end
+        CreateMapMarkerUI()
+        if not initialized then
+            if MoreMapMarkersDB.FrameVisible then
+                MoreMapMarkerFrame:Show()
+            else
+                MoreMapMarkerFrame:Hide()
+            end
+        end
     elseif event == "PLAYER_ENTERING_WORLD" then
         -- Ensure everything is set up when entering world
         if not initialized then
             initialized = true
+            playerfaction = UnitFactionGroup("player")
         end
-        -- Always update markers when entering world
         UpdateMarkers()
     elseif event == "WORLD_MAP_UPDATE" then
         if initialized then
@@ -327,15 +342,27 @@ frame:SetScript("OnEvent", function()
     end
 end)
 
--- SLASH_MMM1 = "/Moar"
--- SlashCmdList["Moar"] = function()
---     if MoarMarkerInfo and MoarMarkerInfo:IsVisible() then
---         MoarMarkerInfo:Hide()
---     else
---         MoarMarkerInfo:Show()
---     end
--- end
-
-if debug then
-    DEFAULT_CHAT_FRAME:AddMessage("Modern Map Markers: Initial Load Complete")
+local function HandleMoreMapMarkersSlashCommand(msg)
+    if msg == "on" or msg == "show" then
+        MoreMapMarkerFrame:Show()
+        MoreMapMarkersDB.FrameVisible = true
+    elseif msg == "off" or msg == "hide" then
+        MoreMapMarkerFrame:Hide()
+        MoreMapMarkersDB.FrameVisible = false
+    elseif msg == "reset" then
+        MoreMapMarkersDB.framePosition = nil
+        MoreMapMarkerFrame:ClearAllPoints()
+        MoreMapMarkerFrame:SetPoint("CENTER", UIParent, "CENTER", 0, 0)
+        MoreMapMarkerFrame:Show()
+        DEFAULT_CHAT_FRAME:AddMessage("MoreMapMarkers: Position reset")
+    else
+        DEFAULT_CHAT_FRAME:AddMessage("/moremm [option] or /moremapmarkers [option]")
+        DEFAULT_CHAT_FRAME:AddMessage("[on] or [show] - shows the frame")
+        DEFAULT_CHAT_FRAME:AddMessage("[off] or [hide] - hides the frame")
+        DEFAULT_CHAT_FRAME:AddMessage("[reset] - resets frame position to center screen")
+    end
 end
+
+SLASH_MOREMAPMARKERS1 = "/moremm"
+SLASH_MOREMAPMARKERS2 = "/moremapmarkers"
+SlashCmdList["MOREMAPMARKERS"] = HandleMoreMapMarkersSlashCommand
